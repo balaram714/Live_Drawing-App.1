f2b813598e614c315c8982778bece4da03f68eb7
Inspiron <nityanandasamanta96@gmail.com>
2020-06-24 10:38:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/livedrawing1/Particle.java
@@ -0,0 +1,36 @@
+package com.example.livedrawing1;
+
+import android.graphics.PointF;
+
+ public class Particle {
+    PointF mVelocity;
+    PointF mPosition;
+
+    Particle(PointF direction)
+    {
+        mVelocity = new PointF();
+        mPosition = new PointF();
+
+        // Determine the direction
+        mVelocity.x = direction.x;
+        mVelocity.y = direction.y;
+    }
+
+    void update(float fps)
+    {
+        // Move the particle
+        mPosition.x += mVelocity.x;
+        mPosition.y += mVelocity.y;
+    }
+
+    void setPosition(PointF position)
+    {
+        mPosition.x = position.x;
+        mPosition.y = position.y;
+    }
+
+    PointF getPosition()
+    {
+        return mPosition;
+    }
+}
